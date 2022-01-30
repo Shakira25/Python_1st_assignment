8e28840a37bbacf3f70193a78d6fd4487902c72b
Shakira Babirye <sbabirye@baylor.local>
2022-01-30 21:21:55 +0300

    add local python file

--- test_python.py
+++ test_python.py
@@ -1,5 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sales = pd.read_csv('sales.csv')
+
 def print_most_profitable_item2():
     y = sales.groupby(['Trade Name'],as_index=True).sum()
     x = y.sort_values(['Profit'], ascending = False)
     w = x.iloc[0].to_frame()
     print(w)
+
+
+print_most_profitable_item2()
